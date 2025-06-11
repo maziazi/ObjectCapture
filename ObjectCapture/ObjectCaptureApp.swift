ed5e45e69d289033a0ebf6cbbfedadbca1bffae7
maziazi <mazis9651@gmail.com>
2025-06-11 23:58:21 +0700

    Initial Commit

---
+++ ObjectCapture/ObjectCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectCaptureApp.swift
+//  ObjectCapture
+//
+//  Created by Muhamad Azis on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ObjectCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObjectCaptureTests/ObjectCaptureTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectCaptureTests.swift
+//  ObjectCaptureTests
+//
+//  Created by Muhamad Azis on 11/06/25.
+//
+
+import Testing
+@testable import ObjectCapture
+
+struct ObjectCaptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
